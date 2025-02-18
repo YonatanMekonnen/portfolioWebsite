37d2ae35d92279146bcbba1e21068fa052e6d768
Yonatan Mekonnen <yonatanmekonnen25@gmail.com>
2025-02-18 23:36:19 +0300

    Image loading issue fix

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import { Container } from "postcss";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#312a2a] container mx-auto px-12 py-4">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div class="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
